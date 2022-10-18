9d9c2c6930a60133bb5bd716e985a183d0adbbb9
Jan Ellenberger <jellenberger1999@gmail.com>
2022-10-18 12:22:50 +0200

    first commit

---
+++ helloWorld.java
@@ -0,0 +1,33 @@
+
+
+public class helloWorld {
+
+    public static void main(String[] args) {
+
+        
+        // Here I'm defining my variables first
+
+        String s = "Hello World";
+        int winner = 2;
+
+        /* 
+         *Here
+         *
+         * Shwoing
+         * You
+         * how 
+         * to 
+         * pritn 
+         * hello world
+        */
+
+        System.out.println(s);
+
+
+        System.out.println(winner);
+       
+        
+    }
+
+    
+}
---
+++ partTwo.java
@@ -0,0 +1,77 @@
+
+
+public class partTwo {
+
+     /*
+         * int val1 = 2;
+         * int val2 = -2;
+         * double val3 = 2.0;
+         * double val7 = -2.1;
+         * char val4 = 'c';
+         * char val6 = '5';
+         * char val8 = '8';
+         * 
+         * boolean is_it_true = false;
+         * 
+         * double num = val1;
+         * 
+         * 
+         * x += y      x = x + y
+         *
+         * x -= y      x = x - y
+         *
+         * x *= y      x = x * y
+         *
+         * x /= y      x = x / y
+         *
+         * 
+         * 
+         */
+
+
+         
+
+    public static void main(String[] args) {
+
+        int val1 = 2;
+        int val2 = 3;
+        double val3 = 2.0;
+        double val4 = 3.0;
+        int sum = val1 + val2;
+        int sum1 = val1 - val2;
+        int sum2 = val1 * val2;
+        double sum3 = val3 / val4;
+        String val5 = "a";
+        String val6 = "b";
+
+        int count = 0;
+        count = count + 1; // count = 1
+
+        count++; //count =2
+        count += 1; //count = 3
+
+        int sum4 = 3 % 2;
+        sum4 --;
+        sum4 += val3;
+        String val7 = val5 + val6;
+
+       System.out.println(sum);
+       System.out.println(sum1);
+       System.out.println(sum2);
+       System.out.println(sum3);
+       System.out.println(count);
+       System.out.println(sum4);
+       System.out.println(val7);
+
+
+
+
+
+
+
+
+        
+    }
+
+    
+}
